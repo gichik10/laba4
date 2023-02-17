da57e445fa6e41673c9b98d3b0ea40e29bbce526
gichik10 <125662520+gichik10@users.noreply.github.com>
2023-02-17 19:08:51 +0600

    Add files via upload

---
+++ laba4_1.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int massiv[10];
+	int c = 9;
+	int sum = 0;
+	for (int i = 0; i < c; i++) {
+		massiv[i] = rand() % 100;
+		cout << massiv[i] << " ";
+		sum += massiv[i];
+		
+	}
+	cout << " ";
+	cout <<"sum:"<< sum << " ";
+	int max = massiv[0];
+	int min = massiv[0];
+	for (int i = 0; i < c; i++) {
+		if (massiv[i] < min) {
+			min = massiv[i];
+		}
+		if(massiv[i]>max){
+			max = massiv[i];
+
+
+		}
+	}
+	cout << " ";
+	cout << "min:" << min;
+	cout << " ";
+	cout << "max:" << max;
+
+
+
+
+
+
+
+}
+
