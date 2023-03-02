118af4057f2b4a115fb4bc5943c0448225275c02
gichik10 <125662520+gichik10@users.noreply.github.com>
2023-03-02 22:36:23 +0600

    Add files via upload

---
+++ laba4_8.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int massiv[] = { 1,4,1,4,5,6,7,8,66,44,1 };
+	int massiv2[11];
+	cout << " sort arr:  ";
+	int c = 0;
+	for (int i = 0; i < 11; i++) {
+		for (int j = 0; j < 11; j++) {
+			if (i != j) {
+				if (massiv[i] == massiv[j]) {
+					c = 1;
+				}
+
+			}
+		}
+		if (c == 1) {
+		}
+		else {
+			cout << " " << massiv[i];
+		}
+		c = 0;
+	}
+	return 0;
+}
